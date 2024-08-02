428afc75fa36ed3c7a81c71a381f1aaa2478e3b8
Sebastian Peñaherrera <sebas172010@outlook.es>
2024-08-02 18:56:03 -0500

    cambios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesTableSeeder::class,
             AdminUserSeeder::class,
-            EmpresaGestoraSeeder::class,
         ]);
     }
 }
